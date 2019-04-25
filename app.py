2aade6a7952281a4185911fd20ab3c0fc4c9ad7b
aliochka <romain.chaffal@gmail.com>
2019-04-25 14:20:11 +0200

    connect gsheet

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print ("Hello, World!")
+from gsheet import read_gsheet
+
+def main():
+    print(read_gsheet('Feuille 1!A1:B2'))
+
+if __name__ == '__main__':
+    main()
---
+++ gsheet.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import os
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+def read_gsheet(range_name):
+    # """Shows basic usage of the Sheets API.
+    # Prints values from a sample spreadsheet.
+    # """
+
+    creds = None
+    # The file credentials/token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('credentials/token.pickle'):
+        with open('credentials/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials/credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('credentials/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    SPREADSHEET_ID = os.environ.get("PC_PUBLI_MAILING_SPREADSHEET_ID")
+    RANGE_NAME = range_name
+
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        return values
+
+
+def write_gsheet(values, spreadsheetId, range_name, valueInputOption='RAW'):
+    creds = None
+    # The file credentials/token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('credentials/token.pickle'):
+        with open('credentials/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials/credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('credentials/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    SPREADSHEET_ID = App.config("PC_PUBLI_MAILING_SPREADSHEET_ID")
+    RANGE_NAME = range_name
+
+    body = {
+        'values': values
+    }
+    result = service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
+        valueInputOption=valueInputOption, body=body).execute()
+    print('{0} cells updated.'.format(result.get('updatedCells')));
+    print (RANGE_NAME)
+
