670d39ea2c0697fcfaad0e86f3255c1455f77563
aliochka <romain.chaffal@gmail.com>
2019-04-25 15:13:01 +0200

    change mail recipient

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from mailjet import send_mail
 from mails import make_mail_test
 
 def main():
-    print(send_mail("romain.chaffal@passculture.app", make_mail_test()))
+    print(send_mail("romain.chaffalpassculture.app", make_mail_test()))
     print(read_gsheet('Feuille 1!A1:B2'))
 
 if __name__ == '__main__':
